84e2d87f6f6be5fca35772b31c85ab8b53554623
xhanyifeng <xhanyifeng@gmail.com>
2017-08-03 16:57:31 +0800

    fix jenkins.sh

--- jenkins-cbs/jenkins.sh
+++ jenkins-cbs/jenkins.sh
@@ -2,7 +2,7 @@
 
 
 : "${JENKINS_HOME:="/var/jenkins_home"}"
-chown jenkins.jenkins "JENKINS_HOME"
+chown jenkins.jenkins "$JENKINS_HOME"
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ \( -type f -o -type l \) -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
